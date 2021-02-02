514b15db25b475fe2fe82fc35696f1be0fdd617a
Sam Ruffle Coles <sam@rufflecol.es>
2021-02-02 10:49:36 +0000

    use Parcelize

--- app/src/main/java/es/rufflecol/sam/breakingbad/data/repository/entity/CharacterEntity.kt
+++ app/src/main/java/es/rufflecol/sam/breakingbad/data/repository/entity/CharacterEntity.kt
@@ -1,12 +1,13 @@
 package es.rufflecol.sam.breakingbad.data.repository.entity
 
-import android.os.Parcel
 import android.os.Parcelable
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import kotlinx.android.parcel.Parcelize
 
 @Entity(tableName = "characters")
+@Parcelize
 data class CharacterEntity(
     @ColumnInfo(name = "character_id") @PrimaryKey val id: Int,
     @ColumnInfo(name = "name") val name: String,
@@ -15,40 +16,4 @@ data class CharacterEntity(
     @ColumnInfo(name = "status") val status: String,
     @ColumnInfo(name = "nickname") val nickname: String,
     @ColumnInfo(name = "series_appearances") val seriesAppearances: String,
-) : Parcelable {
-
-    constructor(parcel: Parcel) : this(
-        parcel.readInt(),
-        parcel.readString().orEmpty(),
-        parcel.readString().orEmpty(),
-        parcel.readString().orEmpty(),
-        parcel.readString().orEmpty(),
-        parcel.readString().orEmpty(),
-        parcel.readString().orEmpty()
-    ) {
-    }
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeInt(id)
-        parcel.writeString(name)
-        parcel.writeString(image)
-        parcel.writeString(occupation)
-        parcel.writeString(status)
-        parcel.writeString(nickname)
-        parcel.writeString(seriesAppearances)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<CharacterEntity> {
-        override fun createFromParcel(parcel: Parcel): CharacterEntity {
-            return CharacterEntity(parcel)
-        }
-
-        override fun newArray(size: Int): Array<CharacterEntity?> {
-            return arrayOfNulls(size)
-        }
-    }
-}
+) : Parcelable
